b30ff1ffa86bf12a5114e4fca39c2af6380ea1c8
Alexey Alyoshin <fingersi@mail.ru>
2024-05-04 20:21:47 +0300

    style fixes and rubocop installed

--- db/migrate/20240503170659_create_answers.rb
+++ db/migrate/20240503170659_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
-      t.text :body
+      t.texte :body
       t.references :question, null: false, foreign_key: true
 
       t.timestamps
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,5 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+  
 end
